220995e58d8356c4dc92c56bba2a1b3b863f76ac
Muhammad Yunus <turmudi.yunus@gmail.com>
2021-05-28 15:56:02 +0700

    Add whatsapp, phone, and email in about page

--- lib/ui/pages/about_page.dart
+++ lib/ui/pages/about_page.dart
@@ -10,99 +10,168 @@ class AboutPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Stack(
-              children: [
-                Container(
-                  color: Color(0xFF00A3FF).withOpacity(0.2),
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
+            Stack(children: [
+              Container(
+                color: Color(0xFF00A3FF).withOpacity(0.2),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                margin: EdgeInsets.only(top: 160),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20)),
+                  color: Colors.white,
                 ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  margin: EdgeInsets.only(top: 160),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-                    color: Colors.white,
-                  ),
-                ),
-                SafeArea(
+              ),
+              SafeArea(
                   child: Padding(
-                    padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.all(8.0),
+                child: Column(children: [
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
+                        child: InkWell(
+                          child: Icon(
+                            Icons.arrow_back,
+                            size: 24,
+                          ),
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      )
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 40),
+                    child: Center(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(25),
+                        child: Image.asset('lib/assets/images/yunus_pic.png',
+                            width: 120, height: 120, fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 13),
                     child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Muhammad Yunus\nTurmudi",
+                          style: titleTextStyle.copyWith(
+                              fontSize: 22, fontWeight: FontWeight.w500),
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Mobile App Developer',
+                          style: valueTextStyle,
+                          textAlign: TextAlign.center,
+                        ),
+                        SizedBox(
+                          height: 30,
+                        ),
+                        // Contact Me
                         Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             Container(
-                              padding: EdgeInsets.fromLTRB(5, 2, 5, 2),
-                              child: InkWell(
-                                child: Icon(Icons.arrow_back, size: 24,),
-                                onTap: () { Navigator.pop(context); },
-                              ),
-                            )
+                              width: 95,
+                              child: Column(children: [
+                                InkWell(
+                                  onTap: () => _launchWhatsApp(context),
+                                  child: Icon(Icons.message)
+                                ),
+                              ]),
+                            ),
+                            Container(
+                              width: 95,
+                              child: Column(children: [
+                                InkWell(
+                                  onTap: () => _launchTel(context),
+                                  child: Icon(Icons.call),
+                                ),
+                              ]),
+                            ),
+                            Container(
+                              width: 95,
+                              child: Column(children: [
+                                InkWell(
+                                  onTap: () => _launchEmail(context),
+                                  child: Icon(Icons.email)
+                                ),
+                              ]),
+                            ),
                           ],
                         ),
-                        Container(
-                          margin: EdgeInsets.only(top: 40),
-                          child: Center(
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(25),
-                              child: Image.asset('lib/assets/images/yunus_pic.png', width: 120, height: 120, fit: BoxFit.fill),
-                            ),
-                          ),
+                        SizedBox(
+                          height: 30,
                         ),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 13),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: [
-                              SizedBox(height: 10,),
-                              Text("Muhammad Yunus\nTurmudi", style: titleTextStyle.copyWith(fontSize: 22, fontWeight: FontWeight.w500), textAlign: TextAlign.center,),
-                              SizedBox(height: 10,),
-                              Text('Mobile App Developer', style: valueTextStyle, textAlign: TextAlign.center,),
-                              SizedBox(height: 30,),
-                              // Additional info
-                              Text('About', style: labelTextStyle),
-                              SizedBox(height: 10,),
-                              Text('Experienced Mobile Engineer with a demonstrated history of working in the information technology and services industry.', style: valueTextStyle),
-                              SizedBox(height: 10,),
-                              Text('Location', style: labelTextStyle),
-                              SizedBox(height: 10,),
-                              Text('Bandung, West Java, Indonesia', style: valueTextStyle),
-                              SizedBox(height: 10,),
-                              Text('Email', style: labelTextStyle),
-                              SizedBox(height: 10,),
-                              Text('turmudi.yunus@gmail.com', style: valueTextStyle),
-                              SizedBox(height: 10,),
-                              Text('Phone', style: labelTextStyle),
-                              SizedBox(height: 10,),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text('0858 7132 xxxx', style: valueTextStyle),
-                                  SizedBox(width: 10,),
-                                  InkWell(
-                                    onTap: () => _launchURL(context),
-                                    child: Image.asset('lib/assets/images/whatsapp.png', width: 26, height: 26, fit: BoxFit.cover,),
-                                  )
-                                ],
-                              )
-                            ],
-                          ),
-                        )
-                      ]
+                        // Additional info
+                        Text('About', style: labelTextStyle),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                            'Experienced Mobile Engineer with a demonstrated history of working in the information technology and services industry.',
+                            style: valueTextStyle),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Location', style: labelTextStyle),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Bandung, West Java, Indonesia',
+                            style: valueTextStyle),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Email', style: labelTextStyle),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('turmudi.yunus@gmail.com', style: valueTextStyle),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Phone', style: labelTextStyle),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('0858 7132 xxxx', style: valueTextStyle),
+                      ],
                     ),
                   )
-                ),
-              ]
-            ),
+                ]),
+              )),
+            ]),
           ],
         ),
       ),
     );
   }
 
-  void _launchURL(context) async => await canLaunch('whatsapp://send?phone=085871321037') ? await launch('whatsapp://send?phone=085871321037') : ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    content: Text("Cannot open whatsapp://send?phone=085871321037"),
-  ));
+  void _launchWhatsApp(context) async => await canLaunch('whatsapp://send?phone=085871321037') ?
+    await launch('whatsapp://send?phone=085871321037') :
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Cannot open WhatsApp")));
+
+  void _launchTel(context) async => await canLaunch("tel://085871321037") ?
+    await launch("tel://085871321037") :
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Cannot open Phone Call")));
+
+  void _launchEmail(context) async => await canLaunch('mailto:yunus8889@gmail.com') ?
+    await launch('mailto:yunus8889@gmail.com') :
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Cannot open Email")));
 }
